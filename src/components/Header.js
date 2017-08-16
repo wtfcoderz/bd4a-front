034b4c8299ef1d572b089e28c63ccda93b2ccf5c
tyrsensei <tyrsensei@gmail.com>
2017-08-16 07:55:48 +0200

    Remove material-ui because of beta version of dependencies

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,29 +1,10 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-import AppBar from 'material-ui/AppBar'
-import Toolbar from 'material-ui/Toolbar'
-import Button from 'material-ui/Button'
-import Typography from 'material-ui/Typography'
-import IconButton from 'material-ui/IconButton'
-import MenuIcon from 'material-ui-icons/Menu'
 
 export default class Header extends React.Component {
   render() {
     return (
       <header>
-        <AppBar>
-          <Toolbar>
-            <IconButton color="inherit" aria-label="Menu">
-              <MenuIcon />
-            </IconButton>
-            <Typography type="title" color="inherit" style={{flex: 1}}>
-              BD4A
-            </Typography>
-            <Button color="inherit" component={Link} to="/login">
-              Login
-            </Button>
-          </Toolbar>
-        </AppBar>
       </header>
     )
   }
--- src/pages/Register.js
+++ src/pages/Register.js
@@ -1,11 +1,39 @@
 import React from "react"
+// import { Link } from 'react-router-dom'
 
 export default class Register extends React.Component {
+  constructor(props) {
+    super(props)
+    this.state = {
+      email: '',
+      email_check: '',
+      password: '',
+      password_check: '',
+    }
+  }
+  onRegister() {
+    console.log('register')
+  }
+
+  validateEmail() {
+    console.log('validate mail')
+  }
+
+  validateEmailVerification() {
+    console.log('verify email')
+    this.validateEmail()
+  }
+
+  handleInputChange(ev) {
+    let target = ev.target
+    this.setState({
+      [target.id]: target.value
+    })
+  }
+
   render () {
     return (
-      <div>
-        Register Page
-      </div>
+      <div></div>
     )
   }
 }
