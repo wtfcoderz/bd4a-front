66b2e45db612c0573c50f970c8c58ef5fd6c7b18
tyrsensei <tyrsensei@gmail.com>
2017-09-12 11:06:09 +0200

    Some cleaning

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,15 +1,30 @@
 import React from 'react'
 // import { Link } from 'react-router-dom'
-// import {
-//   Menu,
-//   Container,
-//   Button
-// } from 'semantic-ui-react'
+import {
+  Segment,
+  Container,
+  Header
+} from 'semantic-ui-react'
 
 export default class Home extends React.Component {
   render() {
     return (
-      <div>Home</div>
+      <div>
+        <Segment
+          inverted
+          textAlign='center'
+          style={{ minHeight: 700, padding: '1em 0em' }}
+          vertical>
+          <Container text textAlign='center'>
+            <Header
+              as='h1'
+              content='Big Data for All'
+              style={{ fontSize: '4em', fontWeight: 'normal', marginBottom: 0, marginTop: '3em' }}
+              inverted
+              />
+          </Container>
+        </Segment>
+      </div>
     )
   }
 }
